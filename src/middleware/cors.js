1a6ac5d81c926fa8cb3c6f5a5e6cb0bf453aa3b2
bnates <brian.nations@codefellows.com>
2017-09-12 14:43:36 -0700

    added cors

---
+++ src/middleware/cors.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default (req, res, next) => {
+  var origin = req.get('origin');
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
